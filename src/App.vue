9034bcb310c04d4164916c390031f6340c4cd63c
Roeurn Rors <roeurngithub26@gmail.com>
2026-07-04 10:11:44 +0700

    Initial Vue 3 project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container mt-5">
+    <h1 class="text-primary">Student Score Management System</h1>
+
+    <button class="btn btn-success">
+      Bootstrap 5 Working
+    </button>
+
+    <i class="bi bi-person-circle fs-2 ms-3"></i>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
