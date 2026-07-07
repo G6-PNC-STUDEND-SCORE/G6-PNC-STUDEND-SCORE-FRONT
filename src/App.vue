b80c244947c2219ac9c36aafb91d2084bb6bcfbd
REAM.KHORN <REAM.KHORN@PNC.LOCAL>
2026-07-07 14:20:48 +0700

    commit my work

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,8 @@
 <template>
-  <div class="app-layout">
-    <Navigation v-if="showSidebar" />
-    <main class="main-content" :class="{ 'with-sidebar': showSidebar }">
-      <router-view />
-    </main>
-  </div>
+  <router-view />
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
-import { useRoute } from 'vue-router'
-import Navigation from './components/Navigation.vue'
-
-const route = useRoute()
-
-const publicPages = ['login']
-const showSidebar = computed(() => !publicPages.includes(String(route.name)))
 </script>
 
 <style>
@@ -27,20 +14,5 @@ const showSidebar = computed(() => !publicPages.includes(String(route.name)))
 
 html, body {
   height: 100%;
-}
-
-.app-layout {
-  display: flex;
-  min-height: 100vh;
-}
-
-.main-content {
-  flex: 1;
-  min-height: 100vh;
-  background: #f0f2f5;
-}
-
-.main-content.with-sidebar {
-  padding-left: 220px;
 }
 </style>
